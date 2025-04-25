683c7b4d1e1be45c643559f722745bd99320a7ca
Nathan <natanribeirobarbosa4161@gmail.com>
2025-04-25 12:04:55 -0300

    first2

--- script.js
+++ script.js
@@ -4,30 +4,6 @@ let canvas = document.createElement('canvas');
 let context = canvas.getContext('2d');
 
 
-
-function saveToCookie(data) {
-  let stored = getStoredQRCodes();
-  if (!stored.includes(data)) {
-    stored.push(data);
-    document.cookie = `qrcodes=${JSON.stringify(stored)};path=/`;
-    updateList();
-  }
-}
-
-function getStoredQRCodes() {
-  let match = document.cookie.match(/(?:^|; )qrcodes=([^;]*)/);
-  return match ? JSON.parse(decodeURIComponent(match[1])) : [];
-}
-
-function updateList() {
-  qrList.innerHTML = '';
-  getStoredQRCodes().forEach(code => {
-    let li = document.createElement('li');
-    li.textContent = code;
-    qrList.appendChild(li);
-  });
-}
-
 function exportToExcel() {
   let codes = getStoredQRCodes();
   let ws = XLSX.utils.aoa_to_sheet([["Código QR"], ...codes.map(code => [code])]);
@@ -41,7 +17,7 @@ function startBarcodeScanner() {
       inputStream: {
         name: "Live",
         type: "LiveStream",
-        target: document.querySelector('#preview'), // o mesmo vídeo do QR
+        target: video,
         constraints: {
           facingMode: "environment"
         },
@@ -74,4 +50,4 @@ function startBarcodeScanner() {
   }
   
 
-updateList();
+
